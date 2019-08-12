2f1a47266ddf1d0ff3fddbd46206e688afa88052
Dana batali <dana.batali@gmail.com>
2019-08-11 17:03:14 -0700

    New Hyperlink style option for DragInt and friends.

--- src/widgets/drag.js
+++ src/widgets/drag.js
@@ -15,6 +15,7 @@ export var DragFlags =
 {
     None: 0,
     Vertical: 1,
+    AsHyperText: 2
 };
 
 const FFmt = "%.3f";
@@ -36,22 +37,22 @@ export var ImguiDragMixin =
     //   move by 5 pixels to increase value by 1). For gamepad/keyboard
     //   navigation, minimum speed is Max(v_speed, minimum_step_at_given_precision).
     DragFloat(label, val, v_speed=1, v_min=0, v_max=0, format=null,
-            power=1, onChange=null)
+            power=1, onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=FFmt;
         return this.DragScalar(label, DataType.Float, val, v_speed, v_min, v_max,
-                            format, power, onChange);
+                            format, power, onChange, onDone, flags);
     },
 
     DragVec2(label, val, v_speed=1, v_min=0, v_max=0, format=null,
-            power=1, onChange=null)
+            power=1, onChange=null, onDone=null, flags=0)
     {
         let value_changed = this.DragFloat2(label, [val.x, val.y],
                                         v_speed, v_min, v_max, format,
                                         power, (newval => {
                                             val.x = newval[0];
                                             val.y = newval[1];
-                                        }));
+                                        }), onDone, flags);
         if(value_changed && onChange)
             onChange(val);
         return value_changed;
@@ -59,23 +60,25 @@ export var ImguiDragMixin =
 
     // If v_min >= v_max we have no bound
     DragFloat2(label, val, v_speed=1, v_min=0, v_max=0, format=null,
-                power=1, onChange=null)
+                power=1, onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=FFmt;
         return this.DragScalarN(label, DataType.Float, val, 2,
-                                v_speed, v_min, v_max, format, power, onChange);
+                                v_speed, v_min, v_max, format, power, 
+                                onChange, onDone, flags);
     },
 
     DragFloat3(label, val, v_speed=1, v_min=0, v_max=0, format=null,
-                power=1, onChange=null)
+                power=1, onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=FFmt;
         return this.DragScalarN(label, DataType.Float, val, 3,
-                                v_speed, v_min, v_max, format, power, onChange);
+                                v_speed, v_min, v_max, format, power, 
+                                onChange, onDone, flags);
     },
 
     DragFloat4(label, val, v_speed=1, v_min=0, v_max=0, format=null,
-                power=1, onChange=null)
+                power=1, onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=FFmt;
         if(val.AsArray != undefined)
@@ -88,17 +91,19 @@ export var ImguiDragMixin =
                                 val.CopyArray(newval);
                                 if(onChange)
                                     onChange(val);
-                            });
+                            }, onDone, flags);
         }
         else
         {
             return this.DragScalarN(label, DataType.Float, val, 4,
-                                v_speed, v_min, v_max, format, power, onChange);
+                                v_speed, v_min, v_max, format, power, 
+                                onChange, onDone, flags);
         }
     },
 
     DragFloatRange2(label, v_current_min, v_current_max, v_speed=1,
-                v_min=0, v_max=0, format=null, format_max=null, power=1, onChange)
+                v_min=0, v_max=0, format=null, format_max=null, power=1, 
+                onChange=null, onDone=null, flags=0)
     {
         let win = this.getCurrentWindow();
         if (win.SkipItems)
@@ -112,14 +117,16 @@ export var ImguiDragMixin =
         let value_changed = this.DragFloat("##min", v_current_min, v_speed,
             (v_min >= v_max) ? -Number.MAX_VALUE : v_min,
             (v_min >= v_max) ? v_current_max : Math.min(v_max, v_current_max),
-            format, power, (newval)=>v_current_min = newval);
+            format, power, 
+            (newval)=>v_current_min = newval, onDone, flags);
 
         this.PopItemWidth();
         this.SameLine(0, g.Style.ItemInnerSpacing.x);
         value_changed |= this.DragFloat("##max", v_current_max, v_speed,
             (v_min >= v_max) ? v_current_min : Math.max(v_min, v_current_min),
             (v_min >= v_max) ? Number.MAX_VALUE : v_max,
-            format_max ? format_max : format, power, (newval)=>v_current_max=newval);
+            format_max ? format_max : format, power, 
+            (newval)=>v_current_max=newval, onDone, flags);
         this.PopItemWidth();
         this.SameLine(0, g.Style.ItemInnerSpacing.x);
 
@@ -134,41 +141,47 @@ export var ImguiDragMixin =
         return value_changed;
     },
 
-    DragInt(label, v, v_speed=1, v_min=0, v_max=0, format=null, onChange=null)
+    DragInt(label, v, v_speed=1, v_min=0, v_max=0, format=null, 
+            onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=IFmt;
         return this.DragScalar(label, DataType.S32, v, v_speed, v_min, v_max,
-                            format, 1, onChange);
+                            format, 1, onChange, onDone, flags);
     },
 
     // If v_min >= v_max we have no bound
-    DragInt2(label, v, v_speed=1, v_min=0, v_max=0, format=null, onChange=null)
+    DragInt2(label, v, v_speed=1, v_min=0, v_max=0, format=null, 
+            onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=IFmt;
         return this.DragScalarN(label, DataType.S32, v, 2, v_speed, v_min, v_max,
-                            format, 1, onChange);
+                            format, 1, onChange, onDone, flags);
     },
 
     // If v_min >= v_max we have no bound
-    DragInt3(label, v, v_speed=1, v_min=0, v_max=0, format=null, onChange=null)
+    DragInt3(label, v, v_speed=1, v_min=0, v_max=0, format=null, 
+            onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=IFmt;
         return this.DragScalarN(label, DataType.S32, v, 3, v_speed, v_min, v_max,
-                            format, 1, onChange);
+                            format, 1, onChange, onDone, flags);
     },
 
     // If v_min >= v_max we have no bound
-    DragInt4(label, v, v_speed=1, v_min=0, v_max=0, format=null, onChange=null)
+    DragInt4(label, v, v_speed=1, v_min=0, v_max=0, format=null, 
+                            onChange=null, onDone=null, flags=0)
     {
         if(format==null) format=IFmt;
         return this.DragScalarN(label, DataType.S32, v, 4, v_speed,
-                                v_min, v_max, format, 1, onChange);
+                                v_min, v_max, format, 1, 
+                                onChange, onDone, flags);
     },
 
     // If v_min >= v_max we have no bound
     DragIntRange2(label, v_current_min, v_current_max,
                   v_speed=1, v_min=0, v_max=0,
-                  format=null, format_max=null, onChange)
+                  format=null, format_max=null, 
+                  onChange=null, onDone=null, flags=0)
     {
         let win = this.getCurrentWindow();
         if (win.SkipItems)
@@ -182,14 +195,14 @@ export var ImguiDragMixin =
             (v_min >= v_max) ? -Number.MAX_SAFE_INTEGER : v_min,
             (v_min >= v_max) ? v_current_max : Math.min(v_max, v_current_max),
             format,
-            (newval)=>v_current_min = newval);
+            (newval)=>{v_current_min = newval;}, onDone, flags);
         this.PopItemWidth();
         this.SameLine(0, g.Style.ItemInnerSpacing.x);
         value_changed |= this.DragInt("##max", v_current_max, v_speed,
             (v_min >= v_max) ? v_current_min : Math.max(v_min, v_current_min),
             (v_min >= v_max) ? Number.MAX_SAFE_INTEGER : v_max,
             format_max ? format_max : format,
-            (newval)=>v_current_max=newval);
+            (newval)=>v_current_max=newval, onDone, flags);
         this.PopItemWidth();
         this.SameLine(0, g.Style.ItemInnerSpacing.x);
 
@@ -205,7 +218,7 @@ export var ImguiDragMixin =
     },
 
     DragScalar(label, data_type, v, v_speed=1, v_min=0, v_max=0, format=null,
-                power=1, onChange=null)
+                power=1, onChange=null, onDone=null, flags=0)
     {
         let win = this.getCurrentWindow();
         if (win.SkipItems)
@@ -222,17 +235,35 @@ export var ImguiDragMixin =
         const id = win.GetID(label);
         const w = this.CalcItemWidth();
         const label_size = this.CalcTextSize(label, true);
-        const frame_bb = new Rect(win.DC.CursorPos,
+        let frame_bb, total_bb;
+        let text_pos;
+        if(flags & DragFlags.AsHyperText)
+        {
+            text_pos = new Vec2(win.DC.CursorPos.x, 
+                        win.DC.CursorPos.y + win.DC.CurrentLineTextBaseOffset);
+
+            frame_bb = new Rect(text_pos, Vec2.AddXY(text_pos, w, label_size.y));
+            total_bb = new Rect(frame_bb.Min,
+                            Vec2.AddXY(frame_bb.Max,
+                                label_size.x>0 ? style.ItemInnerSpacing.x + label_size.x : 0,
+                                0));
+            this.itemSize(total_bb);
+            if (!this.itemAdd(total_bb, id, frame_bb))
+                return false;
+        }
+        else
+        {
+            frame_bb = new Rect(win.DC.CursorPos,
                                    Vec2.AddXY(win.DC.CursorPos,
                                             w, label_size.y+style.FramePadding.y*2));
-        const total_bb = new Rect(frame_bb.Min,
+            total_bb = new Rect(frame_bb.Min,
                                   Vec2.AddXY(frame_bb.Max,
                                     label_size.x>0 ? style.ItemInnerSpacing.x + label_size.x : 0,
                                     0));
-
-        this.itemSize(total_bb, style.FramePadding.y);
-        if (!this.itemAdd(total_bb, id, frame_bb))
-            return false;
+            this.itemSize(total_bb, style.FramePadding.y);
+            if (!this.itemAdd(total_bb, id, frame_bb))
+                return false;
+        }
 
         const hovered = this.itemHoverable(frame_bb, id);
 
@@ -271,32 +302,62 @@ export var ImguiDragMixin =
                                             v = newval;
                                             if(onChange)
                                                 onChange(v);
+                                        },
+                                        function()
+                                        {
+                                            if(onDone)
+                                                onDone();
                                         });
         if (value_changed)
             this.markItemEdited(id);
 
-        // Draw frame
-        const frame_col = style.GetColor(g.ActiveId == id ? "FrameBgActive" :
-                    g.HoveredId == id ? "FrameBgHovered" : "FrameBg");
-        this.renderNavHighlight(frame_bb, id);
-        this.renderFrame(frame_bb.Min, frame_bb.Max, frame_col, true,
-                        style.FrameRounding);
 
-        // Display value using user-provided display format so user can add prefix/suffix/decorations to the value.
+        // Display value using user-provided display format so user 
+        // can add prefix/suffix/decorations to the value.
         let valstr = FormatValues(format, [v]);
-        this.renderTextClipped(frame_bb.Min, frame_bb.Max, valstr, null,
+
+        if(flags & DragFlags.AsHyperText)
+        {
+            const frame_col = style.GetColor(g.ActiveId == id ? "FrameBgActive" :
+                    g.HoveredId == id ? "FrameBgHovered" : "FrameBg");
+            this.renderFrame(frame_bb.Min, frame_bb.Max, frame_col, true,
+                            style.FrameRounding);
+
+            let active = g.ActiveId == id && 
+                         g.ActiveIdSource == InputSource.Mouse && 
+                         g.IO.MouseDown[0];
+
+            let col = style.GetColor((active||hovered) ? "LinkHovered" : "Link");
+            this.PushStyleColor("Text", col);
+            this.renderTextClipped(frame_bb.Min, frame_bb.Max, valstr, null,
+                                new Vec2(0.5, 0.5));
+            this.PopStyleColor();
+        }
+        else
+        {
+            const frame_col = style.GetColor(g.ActiveId == id ? "FrameBgActive" :
+                    g.HoveredId == id ? "FrameBgHovered" : "FrameBg");
+            this.renderNavHighlight(frame_bb, id);
+            this.renderFrame(frame_bb.Min, frame_bb.Max, frame_col, true,
+                            style.FrameRounding);
+
+            // Display value using user-provided display format so user can add prefix/suffix/decorations to the value.
+            this.renderTextClipped(frame_bb.Min, frame_bb.Max, valstr, null,
                                 new Vec2(0.5, 0.5));
 
+        }
         if (label_size.x > 0)
+        {
             this.renderText(new Vec2(frame_bb.Max.x + style.ItemInnerSpacing.x,
-                                     frame_bb.Min.y + style.FramePadding.y),
-                            label);
+                                        frame_bb.Min.y + style.FramePadding.y),
+                                label);
+        }
 
         return value_changed;
     },
 
     DragScalarN(label, data_type, v, components, v_speed=1, v_min=0, v_max=0,
-                format=null, power=1, onChange=null)
+                format=null, power=1, onChange=null, onDone=null, flags=0)
     {
         let win = this.getCurrentWindow();
         if (win.SkipItems)
@@ -315,7 +376,7 @@ export var ImguiDragMixin =
                             {
                                 v[i] = newval;
                                 value_changed = true;
-                            });
+                            }, onDone, flags);
             this.SameLine(0, g.Style.ItemInnerSpacing.x);
             this.PopID();
             this.PopItemWidth();
@@ -332,19 +393,23 @@ export var ImguiDragMixin =
 
     /*-------------------------------------------------------------*/
     // used by drag-text widgets
-    dragBehavior(id, data_type, v, v_speed, v_min, v_max, format, power, flags,
-                onChange)
+    dragBehavior(id, data_type, v, v_speed, v_min, v_max, format, power, 
+                flags, onChange, onDone)
     {
         let g = this.guictx;
         if(g.ActiveId == id)
         {
             if(g.ActiveIdSource == InputSource.Mouse && !g.IO.MouseDown[0])
+            {
                 this.clearActiveID();
+                if(onDone) onDone();
+            }
             else
             if(g.ActiveIdSource == InputSource.Nav &&
                 g.NavActivatePressedId == id && g.ActiveIdIsJustActivated)
             {
                 this.clearActiveID();
+                if(onDone) onDone();
             }
         }
         if(g.ActiveId != id)
